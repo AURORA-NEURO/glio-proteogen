a839f75bf36edada8eae5f682cdb438cbac22432
MurariAmbati <170054497+MurariAmbati@users.noreply.github.com>
2026-08-15 09:20:08 -0500

    style(m11-04): normalize plugin exports

--- src/glio_proteogen/modules/c11_protein_native_subtype/m11_04_network_state_mechanism_inference/__init__.py
+++ src/glio_proteogen/modules/c11_protein_native_subtype/m11_04_network_state_mechanism_inference/__init__.py
@@ -7,17 +7,15 @@ from .engine import (
     infer_variant_peptide_mechanism,
     preflight_mechanism_authorization,
 )
-from .service import (
-    M1104Service,
-)
 from .plugin import M1104Plugin, ValidatedM1104Request
+from .service import M1104Service
 
 __all__ = [
     "M1104MechanismAuthorizationError",
     "M1104MechanismEngine",
+    "M1104Plugin",
     "M1104ReplayVerificationError",
     "M1104Service",
-    "M1104Plugin",
     "ValidatedM1104Request",
     "infer_variant_peptide_mechanism",
     "preflight_mechanism_authorization",
