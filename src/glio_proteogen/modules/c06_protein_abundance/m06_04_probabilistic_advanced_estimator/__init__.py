fcb7facdd2af8223fe3f3c3207547665dd119e9c
MurariAmbati <170054497+MurariAmbati@users.noreply.github.com>
2026-08-15 01:52:49 -0500

    M06-04 add provisional plugin and transport parity

--- src/glio_proteogen/modules/c06_protein_abundance/m06_04_probabilistic_advanced_estimator/__init__.py
+++ src/glio_proteogen/modules/c06_protein_abundance/m06_04_probabilistic_advanced_estimator/__init__.py
@@ -8,14 +8,18 @@ from .engine import (
     estimate_protein_abundance_probabilistic,
     preflight_probabilistic_estimator_authorization,
 )
+from .plugin import M0604Plugin, M0604Submission, ValidatedM0604Request
 from .service import M0604Service
 
 __all__ = [
     "M0604_PROXY_OPTIMIZER",
+    "M0604Plugin",
     "M0604ProbabilisticEstimatorEngine",
     "M0604Service",
+    "M0604Submission",
     "ProbabilisticEstimatorAuthorizationError",
     "ProbabilisticEstimatorInputError",
+    "ValidatedM0604Request",
     "estimate_protein_abundance_probabilistic",
     "preflight_probabilistic_estimator_authorization",
 ]
