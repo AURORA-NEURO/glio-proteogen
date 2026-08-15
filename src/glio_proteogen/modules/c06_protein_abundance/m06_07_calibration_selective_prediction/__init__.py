a61e183a9026cdc6ad5e0c1cdf23b0bc6296d3d3
MurariAmbati <170054497+MurariAmbati@users.noreply.github.com>
2026-08-15 02:17:34 -0500

    feat(m06-07): add plugin api and cli parity

--- src/glio_proteogen/modules/c06_protein_abundance/m06_07_calibration_selective_prediction/__init__.py
+++ src/glio_proteogen/modules/c06_protein_abundance/m06_07_calibration_selective_prediction/__init__.py
@@ -8,14 +8,18 @@ from .engine import (
     calibrate_selective_protein_abundance,
     preflight_calibration_authorization,
 )
+from .plugin import CalibrationSubmission, M0607Plugin, ValidatedM0607Request
 from .service import M0607Service
 
 __all__ = [
     "BuiltCalibration",
     "CalibrationAuthorizationError",
     "CalibrationInputError",
+    "CalibrationSubmission",
     "M0607CalibrationEngine",
+    "M0607Plugin",
     "M0607Service",
+    "ValidatedM0607Request",
     "calibrate_selective_protein_abundance",
     "preflight_calibration_authorization",
 ]
