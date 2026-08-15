c187d6ffd956808d54c41d3da76badbd38985cc4
MurariAmbati <170054497+MurariAmbati@users.noreply.github.com>
2026-08-15 14:42:56 -0500

    feat(m15-04): add strict plugin API and CLI adapters

--- src/glio_proteogen/modules/c15_longitudinal_recurrence/m15_04_network_state_mechanism_inference/__init__.py
+++ src/glio_proteogen/modules/c15_longitudinal_recurrence/m15_04_network_state_mechanism_inference/__init__.py
@@ -8,14 +8,17 @@ from .engine import (
     infer_complex_activity_mechanism,
     preflight_mechanism_authorization,
 )
+from .plugin import M1504Plugin, ValidatedM1504Request
 from .service import M1504Service
 
 __all__ = [
     "M1504AuthorizationError",
     "M1504InferenceError",
     "M1504MechanismInference",
+    "M1504Plugin",
     "M1504ReplayVerificationError",
     "M1504Service",
+    "ValidatedM1504Request",
     "infer_complex_activity_mechanism",
     "preflight_mechanism_authorization",
 ]
