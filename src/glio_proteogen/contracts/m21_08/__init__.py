a462a0e98cf2cce02cc7da1730d6b0d583ca1b60
MurariAmbati <170054497+MurariAmbati@users.noreply.github.com>
2026-08-16 07:01:45 -0500

    Expose M21-08 authority constants

--- src/glio_proteogen/contracts/m21_08/__init__.py
+++ src/glio_proteogen/contracts/m21_08/__init__.py
@@ -15,11 +15,24 @@ from glio_proteogen.contracts.m21_08.schema import (
     contract_json_schemas,
 )
 from glio_proteogen.contracts.m21_08.v1 import *  # noqa: F403
+from glio_proteogen.contracts.m21_08.v1 import (
+    M2108_CONTRACT_VERSION,
+    M2108_DOSSIER_SHA256,
+    M2108_DOSSIER_SLICE,
+    M2108_EVIDENCE_CLAIM,
+    M2108_GATE,
+    M2108_M2106_INPUT_MEDIA_TYPE,
+    M2108_M2107_INPUT_MEDIA_TYPE,
+    M2108_MODULE_ID,
+    M2108_OUTPUT_MEDIA_TYPE,
+    M2108_OWNER,
+    M2108_PARENT,
+    M2108_PROVISIONAL_ABI,
+    M2108_SAFETY_CLASS,
+)
 
 __all__ = [
     "CONTRACT_VERSION",
-    "SCHEMA_ID_PREFIX",
-    "ContractName",
     "M2108_CONTRACT_VERSION",
     "M2108_DOSSIER_SHA256",
     "M2108_DOSSIER_SLICE",
@@ -33,6 +46,8 @@ __all__ = [
     "M2108_PARENT",
     "M2108_PROVISIONAL_ABI",
     "M2108_SAFETY_CLASS",
+    "SCHEMA_ID_PREFIX",
+    "ContractName",
     "canonical_request_digest",
     "contract_json_schema",
     "contract_json_schemas",
