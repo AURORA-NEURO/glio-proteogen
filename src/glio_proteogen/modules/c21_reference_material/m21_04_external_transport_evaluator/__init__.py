261771105281f662d90808bc80708fde9396d193
MurariAmbati <170054497+MurariAmbati@users.noreply.github.com>
2026-08-16 06:38:10 -0500

    Add M21-04 API CLI and plugin interface parity

--- src/glio_proteogen/modules/c21_reference_material/m21_04_external_transport_evaluator/__init__.py
+++ src/glio_proteogen/modules/c21_reference_material/m21_04_external_transport_evaluator/__init__.py
@@ -1,5 +1,7 @@
 """M21-04 deterministic external transport evaluator."""
 
+from .api import create_app
+from .cli import app as cli_app
 from .engine import (
     M2104AuthorizationError,
     M2104Engine,
@@ -17,6 +19,8 @@ __all__ = [
     "M2104ReplayError",
     "M2104Service",
     "ValidatedM2104Request",
+    "cli_app",
+    "create_app",
     "evaluate_complex_activity_external_transport",
     "preflight_m2104_authorization",
 ]
