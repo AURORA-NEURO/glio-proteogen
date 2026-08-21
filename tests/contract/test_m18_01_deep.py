e7c2d20b6fa8539f88c79c95b8afff72956b45a1
MurariAmbati <170054497+MurariAmbati@users.noreply.github.com>
2026-08-21 11:23:54 -0500

    style(m18-01): finalize replay test import

--- tests/contract/test_m18_01_deep.py
+++ tests/contract/test_m18_01_deep.py
@@ -40,9 +40,7 @@ from glio_proteogen.kernel.models import (
     UpstreamDecisionReference,
     UpstreamDecisionState,
 )
-from glio_proteogen.modules.c17_metabolomic_lipidomic_integration.m18_01_upstream_contract_resolver import (
-    M1801Service,
-)
+from glio_proteogen.modules.c17_metabolomic_lipidomic_integration.m18_01_upstream_contract_resolver import M1801Service
 
 _WHEN = datetime(2026, 1, 1, tzinfo=UTC)
 _MEDIA_TYPE = "application/vnd.glio-proteogen.source+json"
